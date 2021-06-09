33d223824643f1a09f61d0973f0d3c0248d37cdf
Luis Genesius <luis.huang321@gmail.com>
2021-06-09 17:10:23 +0700

    add cockpit storyboard and cockpit view controller

---
+++ GitFlowTest/Cockpit/CockpitRoomViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CockpitRoomViewController.swift
+//  GitFlowTest
+//
+//  Created by Luis Genesius on 09/06/21.
+//
+
+import UIKit
+
+class CockpitRoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
